9b16bc4b46802d3502fac1dd30987c7c9ce9a238
BorosFather <boros.zoli024@gmail.com>
2023-04-14 19:49:21 +0200

    screens hozzáadva

---
+++ screens/Home.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+
+ export default function Home({navigation}) {
+    return (
+        <View style={styles.container}>
+          <Text>Open up App.js to start working on your app!</Text>
+          <StatusBar style="auto" />
+        </View>
+      );
+ }
