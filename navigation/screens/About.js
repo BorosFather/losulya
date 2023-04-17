a3fb3ea2d5f1e58d84f1a917007fb392f48d3e98
BorosFather <boros.zoli024@gmail.com>
2023-04-17 10:56:38 +0200

    home page kész

--- navigation/screens/About.js
+++ navigation/screens/About.js
@@ -4,8 +4,10 @@ import { StyleSheet, View, Text } from "react-native";
  export default function About({navigation}) {
     return (
         <View style={styles.container}>
-          <Text onPress={() => navigation.navigate("Home")} 
-          style={{ fontSize: 14, fontWeight: "bold"}} > About</Text>
+          
+          <Text>
+            
+          </Text>
           
         </View>
       );
--- navigation/screens/Home.js
+++ navigation/screens/Home.js
@@ -1,21 +1,63 @@
-import { StyleSheet, View, Text } from "react-native";
+import { useState } from "react";
+import { StyleSheet, View, Text, TextInput, TouchableHighlight } from "react-native";
+import { StatusBar } from 'expo-status-bar';
 
+export default function Home({navigation}) {
 
- export default function Home({navigation}) {
-    return (
-        <View style={styles.container}>
-          <Text onPress={() => navigation.navigate("About")} 
-          style={{ fontSize: 14, fontWeight: "bold"}} > Home</Text>
-          
-        </View>
-      );
-    }
+  const [chestg, setChestg] = useState("");
+  const [length, setLength] = useState("");
+  const [volume, setVolume] = useState("");
 
- const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
+  function calWeight(){
+    let localVolume = (parseInt(chestg) * parseInt(chestg) * parseInt(length)) / 11887;
+    setVolume(localVolume.toString());
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Ló súlyának számítása</Text>
+
+      <Text>Mellkas kerülete</Text>
+      <TextInput style={styles.input} onChangeText={(data) => setChestg(data)}/>
+
+      <Text>Hossz</Text>
+      <TextInput style={styles.input} onChangeText={(data) => setLength(data)}/>
+
+      <TouchableHighlight style={styles.btn} onPress={calWeight}>
+        <Text style={styles.btnText}>Számít</Text> 
+      </TouchableHighlight> 
+
+      <Text>Ló súlya</Text>
+      <TextInput style={styles.input} value={volume} /> 
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    padding: 10,
+    marginVertical: 10,
+    width: '80%',
+  },
+  btn: {
+    backgroundColor: '#2196F3',
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 10,
+  },
+  btnText: {
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+});
